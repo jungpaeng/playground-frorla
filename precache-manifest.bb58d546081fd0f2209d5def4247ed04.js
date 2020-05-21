d57e90afb81899ea67293088dc9074214f441411
jungpaeng <meis1541@naver.com>
2020-05-21 18:52:14 +0900

    Updates

---
+++ precache-manifest.bb58d546081fd0f2209d5def4247ed04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d6ca3cffa5d013e809523adbe81ed5",
+    "url": "/playground-frorla/index.html"
+  },
+  {
+    "revision": "fcb0fe0cb661e9799c25",
+    "url": "/playground-frorla/static/css/2.bd089317.chunk.css"
+  },
+  {
+    "revision": "fcb0fe0cb661e9799c25",
+    "url": "/playground-frorla/static/js/2.0914a3c8.chunk.js"
+  },
+  {
+    "revision": "44c77d4f65aca2a11c8b6b50158922dc",
+    "url": "/playground-frorla/static/js/2.0914a3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede34d683db1b092bc4a",
+    "url": "/playground-frorla/static/js/main.fe78f34e.chunk.js"
+  },
+  {
+    "revision": "8057220abb0d8d546f5b",
+    "url": "/playground-frorla/static/js/runtime-main.9d37a200.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/playground-frorla/precache-manifest.bb58d546081fd0f2209d5def4247ed04.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/playground-frorla/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
