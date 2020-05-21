2e80c5aa0df1c25f6b440d4c19e19b5263eebbd6
jungpaeng <meis1541@naver.com>
2020-05-21 18:44:53 +0900

    Add Image, Video Upload to Froala Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
+// @ts-nocheck
 import React from 'react';
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-import FroalaEditorComponent from 'react-froala-wysiwyg';
+import FroalaEditor from 'react-froala-wysiwyg';
+import Froalaeditor from 'froala-editor';
+import 'froala-editor/js/plugins/image.min.js';
+import 'froala-editor/js/plugins/video.min.js';
+import 'froala-editor/js/plugins/file.min.js';
 
-const App: React.FC = () => (
-  <div className="App">
-    <FroalaEditorComponent tag="textarea" />
-  </div>
-);
+Froalaeditor.DefineIcon('alert', { NAME: 'info', SVG_KEY: 'help' });
+Froalaeditor.RegisterCommand('alert', {
+  title: 'Hello',
+  focus: false,
+  undo: false,
+  refreshAfterCallback: false,
+  callback: () => {
+    alert('Hello!');
+  },
+});
+
+const EDITOR_CONFIG = {
+  placeholderText: 'Edit Your Content Here!',
+  pasteAllowLocalImages: true,
+  toolbarButtons: [['undo', 'redo', 'bold', 'insertImage', 'insertVideo', 'insertFile'], ['alert']],
+};
+
+const App: React.FC = () => {
+  const [state, setState] = React.useState('<p>Default Text</p>');
+  const editorRef = React.useRef(null);
+
+  const imageBeforeUpload = React.useCallback((files) => {
+    const { editor } = editorRef.current;
+
+    if (files.length) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        editor.image.insert(e.target.result, null, null, editor.image.get());
+      };
+      reader.readAsDataURL(files[0]);
+    }
+    editor.popups.hideAll();
+    return false;
+  }, []);
+
+  return (
+    <div className="App">
+      <section>
+        <h1>Editor</h1>
+        <FroalaEditor
+          ref={editorRef}
+          tag="textarea"
+          config={{
+            events: {
+              ...EDITOR_CONFIG,
+              'image.beforeUpload': imageBeforeUpload,
+            },
+          }}
+          model={state}
+          onModelChange={setState}
+        />
+      </section>
+      <section>
+        <h1>Renderer</h1>
+        <p>{state}</p>
+      </section>
+    </div>
+  );
+};
 
 export default App;
